91ef05f20e8d51eb5e2099c228a33a8eef721d5f
Gabriel <gchavezmardonez@gmail.com>
2020-04-04 17:40:04 -0400

    Upload image test

--- test.py
+++ test.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     api_key = app.options.get('apiKey')
     image = util.get_image_base64("image.jpg")
     token = auth.get_token(api_key)
-    url = 'http://localhost:5000/shantiapp-4eae1/us-central1/uploadImage'
+    url = 'https://us-central1-shantiapp-4eae1.cloudfunctions.net/uploadImage'
     data = {
         'image': image,
         'location': 'test'
     }
-    result = util.fetch_cloud_functions(token, url, data)
+    result = util.fetch_cloud_functions(token, url, data, fetch_type='get')
     print(result)
+    print(result.get('imagePath'))
     print('End')
